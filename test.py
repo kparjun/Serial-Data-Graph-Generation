1280ab348e099ae7f91b4496948a56c720f00a5e
abrahamjroy <abrahamjroy@users.noreply.github.com>
2017-04-25 23:29:15 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)#Port of your device of choice
 time.sleep(1)
 
-logfile = open('test.csv', 'a')
+logfile = open('test.csv', 'a')#.csv file to which data is stored
 
 while 1:
 	line = ser.readline() 
